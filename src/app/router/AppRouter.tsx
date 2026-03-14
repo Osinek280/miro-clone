ac475fa7b22be229ecad47a8eba5e23feeeae6b3
Szymon <szymon.osinski.2009@gmail.com>
2026-03-14 19:01:28 +0100

    avoid 'is declared but its value is never used' issue by comment

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { PublicRoute } from './PublicRoute';
-import { ProtectedRoute } from './ProtectedRoute';
+// import { ProtectedRoute } from './ProtectedRoute';
 import { AppLayout } from '../layouts/AppLayout';
 import { LoginForm } from '../../features/auth/components/LoginForm';
 import { RegisterForm } from '../../features/auth/components/RegisterForm';
