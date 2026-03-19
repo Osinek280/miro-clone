4c35baeee4291b9e2c197b8957e1b42462d3a3cb
Szymon <szymon.osinski.2009@gmail.com>
2026-03-19 19:36:46 +0100

    Client useEffect fn => useBoardSync.ts

--- src/features/canvas/hooks/useBoardSync.ts
+++ src/features/canvas/hooks/useBoardSync.ts
@@ -1,12 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { canvasApi } from '../api/canvas.api';
 import type { Point } from '../types/types';
 import { useCanvasStore } from './useCanvasStore';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 export function useBoardSync(
   boardId: string,
   setCenterAtPoint: (point: Point, zoom: number) => void,
 ) {
+  const stompClientRef = useRef<Client | null>(null);
+
   useEffect(() => {
     async function loadIntialState() {
       const snapshot = await canvasApi.getSnapshot(boardId);
@@ -27,4 +31,40 @@ export function useBoardSync(
 
     loadIntialState();
   }, [boardId]);
+
+  useEffect(() => {
+    const client = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws') as any,
+      reconnectDelay: 3000,
+      debug: (str) => {
+        console.log('STOMP:', str);
+      },
+
+      onConnect: () => {
+        console.log('connected');
+        client.subscribe('/topic/draw', (msg: any) => {
+          console.log('test');
+          const data = JSON.parse(msg.body);
+          console.log('parsed:', data);
+        });
+      },
+
+      onWebSocketError: (error) => console.error('WebSocket error', error),
+      onDisconnect: () => console.log('disconnected'),
+      onStompError: (frame) => {
+        console.error('STOMP error', frame);
+        console.log('error code', frame.headers['message-type']);
+        console.log('error body', frame.body);
+      },
+    });
+
+    client.activate();
+    stompClientRef.current = client;
+
+    return () => {
+      client.deactivate();
+    };
+  }, []);
+
+  return { stompClientRef };
 }
